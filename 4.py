7a1acd5861b76d95c4b28dbd894025f667f93b34
rishikeshsreehari <14956650+rishikeshsreehari@users.noreply.github.com>
2024-08-25 17:25:34 +0400

    Updated solution to question 4

--- 4.py
+++ 4.py
@@ -6,3 +6,33 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 
 
 '''
+
+import time
+
+start_time = time.time()
+
+max_product = 0
+max = 999
+min = 99
+
+for i in range(max,min,-1):
+    for j in range(max,min,-1):
+        product = i * j
+        if product < max_product:
+            break
+        elif(str(product)) == (str(product))[::-1]:
+            max_product = product
+                
+print(f"The maximum palindrome is {max_product}")
+
+end_time = time.time()
+
+elapsed_time = end_time - start_time
+print(f"Time taken: {elapsed_time} seconds")
+
+            
+            
+
+
+
+
